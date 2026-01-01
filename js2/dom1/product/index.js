b8cb0c2a64b48e4bfb78e4630109fb9a3cfb0cf2
Nitin Gayki <ngayki74@gmail.com>
2026-01-01 11:25:21 +0530

    add

---
+++ js2/dom1/product/index.js
@@ -0,0 +1,48 @@
+let form = document.querySelector("form");
+let tbody=document.querySelector("tbody")
+let product_name=document.getElementById("name");
+let catagory=document.getElementById("catagory");
+let brand=document.getElementById("brand");
+let price=document. getElementById("price");
+let delivered=document.getElementById("deliveredBy");
+
+
+form.addEventListener("submit",getData)
+product_arr=[]
+function getData(){
+    event.preventDefault()
+    //console.log(product_name.value)
+   // console.log(price.value)
+   let data ={
+    name:product_name.value,
+    catagory:catagory.value,
+    brand:brand.value,
+    price:price.value,
+    delivered:delivered.value 
+   }
+   //console.log(data)
+   product_arr.push(data)
+   console.log(product_arr)
+
+
+ // for(let i =0;i<6;i++){
+   let row=document.createElement("tr")
+
+   let col1=document.createElement("td");
+   col1.innerText=data.name
+   let col2=document.createElement("td");
+   col2.innerText=data.catagory
+   let col3=document.createElement("td");
+   col3.innerText=data.brand
+   let col4=document.createElement("td");
+   col4.innerText=data.price
+   let col5=document.createElement("td");
+   col5.innerText=data.delivered
+   let col6=document.createElement("td");
+   col6.innerText=data.price
+   let col7=document.createElement("td");
+   col7.innerText="delete"
+   row.append(col1,col2,col3,col4,col5,col6,col7)
+  tbody.append(row)
+ //  }
+}
